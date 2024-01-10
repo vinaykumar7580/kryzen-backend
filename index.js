a2045c0bdc75eef2627bdca8a4362633a6cb72e1
vinaykumar7580 <vinayhatwar16@gmail.com>
2024-01-10 16:28:22 +0530

    backend

--- index.js
+++ index.js
@@ -34,10 +34,10 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 app.use(express.json());
 app.use(cors())
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   next();
+// });
 
 
 //The below middleware serve static files to frontend.
